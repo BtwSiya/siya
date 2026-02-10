f0d32374cc696a4b796df788b992da0e969d7fac
𝘚𝘪𝘺𝘢 ‹𝟹 <siyatechbot@gmail.com>
2026-02-10 17:35:27 +0530

    Update _inline.py

--- Dev/helpers/_inline.py
+++ Dev/helpers/_inline.py
@@ -38,7 +38,7 @@ class Inline:
                     self.ikb(text="‣‣I", callback_data=f"controls skip {chat_id}"),
                     self.ikb(text="▢", callback_data=f"controls stop {chat_id}"),
                 ],
-                [   self.ikb(text=_lang["close"], callback_data="help close")]
+                [   self.ikb(text="close", callback_data="help close")]
             )
         return self.ikm(keyboard)
 
